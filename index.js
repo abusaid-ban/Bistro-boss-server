4a5b73b1f1b295cbe2a219f73a86baded1e7bb63
unknown <personal.abusaid@gmail.com>
2024-01-24 10:11:27 +0600

    work on delete item

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -52,8 +52,14 @@ async function run() {
 
         app.post("/carts",async (req,res) =>{
             const item = req.body;
-            console.log(item);
+            // console.log(item);
             const result = await cartCollection.insertOne(item);
+            res.send(result);
+        })
+        app.delete("/carts/:id",async (req,res) =>{
+            const id = req.params.id
+           const query = {_id :new ObjectId(id)}
+           const result = await cartCollection.deleteOne(query);
             res.send(result);
         })
 
